03fda750d85580e1253410bc951aa06e96bd0861
Jason Breedlove <j@jasonbreedlove.com>
2025-05-03 18:20:34 -0700

    Refactor Timer class into separate module and update UI.
    Moved the Timer class into a new "timer.js" file for better modularity and maintainability. Updated "index.html" to include a new SVG circle element and ensure scripts load in the correct order. Adjusted callback logic in the Timer class to invoke "onComplete" and "onTick" appropriately.

--- src/index.js
+++ src/index.js
@@ -1,54 +1,16 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callbacks){
-        this.onStart = callbacks.onStart;
-        this.onTick = callbacks.onTick;
-        this.onComplete = callbacks.onComplete;
-    }
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    if(this.onStart){
-        this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  tick = () => {
-    const timeRemaining = this.timeRemaining;
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = timeRemaining - 1;
-    }
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector("#start");
-const pauseButton = document.querySelector("#pause");
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
-    console.log("Timer started");
+    console.log('Timer started');
   },
-  onTick() {},
-  onComplete() {},
+  onTick() {
+    console.log('Timer Ticked');
+  },
+  onComplete() {
+    console.log('Timer completed');
+  },
 });
 
-// color = '#1a1d27'
